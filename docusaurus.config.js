4a8d3d1bcf73b06c42813fa8759b07b1a6e7e3fd
MiracleOmokaro <211651859+MiracleOmokaro@users.noreply.github.com>
2026-07-10 07:31:55 +0000

    fix: TASK 21 - OG image PNG created, absolute URLs with /website/ baseUrl in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,13 +37,15 @@ const config = {
     metadata: [
       { name: 'og:title', content: 'Resolute Raccoon' },
       { name: 'og:description', content: 'A blackhat-grade cybersecurity club. Train. Break. Build.' },
-      { name: 'og:image', content: 'https://resoluteraccoon.github.io/img/logo.svg' },
+      { name: 'og:image', content: 'https://resoluteraccoon.github.io/website/img/og-logo.png' },
+      { name: 'og:image:width', content: '1200' },
+      { name: 'og:image:height', content: '630' },
       { name: 'og:type', content: 'website' },
-      { name: 'og:url', content: 'https://resoluteraccoon.github.io' },
+      { name: 'og:url', content: 'https://resoluteraccoon.github.io/website/' },
       { name: 'twitter:card', content: 'summary_large_image' },
       { name: 'twitter:title', content: 'Resolute Raccoon' },
       { name: 'twitter:description', content: 'A blackhat-grade cybersecurity club. Train. Break. Build.' },
-      { name: 'twitter:image', content: 'https://resoluteraccoon.github.io/img/logo.svg' },
+      { name: 'twitter:image', content: 'https://resoluteraccoon.github.io/website/img/og-logo.png' },
     ],
     navbar: {
       title: 'RESOLUTE RACCOON',
